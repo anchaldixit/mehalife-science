28705a1f89204764d9f435a57ef280d7bfb2c6e4
Anchal <dixitanchal11@gmail.com>
2018-07-10 16:02:44 +0530

    'Plugins

---
+++ wp-content/themes/meha/about-test.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Template Name: about Static Page
+ *
+ * The template for homepage
+ *
+ */
+get_header();
+?>
+<div class="background-about">
+   <div class="about-content">
+   	<h1>Who We Are</h1>
+	    <div class="about-main">
+             <p>Meha Life Sciences is a leading global pharmaceutical company, dedicated to high-quality, branded and generic medicines. We are trusted by healthcare professionals and patients across geographies. Over the last 3 Year, we have strengthened our leadership in India’s pharmaceutical industry and fortified our promise of ‘Caring for Life’.</p>
+         </div>
+    <div class="about-section">
+        <div class="about-row">
+        	<ul>
+                   <li>
+                   		<div class="about-row-shift">
+                       		 <img src="/wp-content/uploads/2018/06/icon1.png">
+                        </div>
+                        	<p>Incorporated</p><br><spam>2015</spam>
+                    </li>
+            </ul>
+        </div>
+        <div class="about-row">
+        	<div class="about-row-shift">
+          	  <img src="/wp-content/uploads/2018/06/icon2.png">
+          	</div>
+                    <p>Managing Director</p><br><spam>Meha Srivastava</spam>
+        </div>
+	</div>
+	<div class="about-member">
+		<div class="about-member-section">
+			<div class="about-member-post">
+				<span>CEO</span>
+			</div>
+			<div class="about-member-name">
+				<p>Mr.Jyoti</p>
+			</div>
+		</div>
+	</div>
+	<div class="about-member">
+		<div class="about-member-section">
+			<div class="about-member-post">
+				<span>PRESIDENT</span>  
+			</div>
+			<div class="about-member-name">
+				<p>Mr.Kamendra Srivastava</p>
+			</div>
+		</div>
+	</div>
+	<div class="about-member">
+		<div class="about-member-section">
+			<div class="about-member-post">
+				<span>H.R</span> 
+			</div>
+			<div class="about-member-name">
+				<p>Mr. Sangeeta</p>
+			</div>
+		</div>
+	</div>
+	<div class="about-member">
+		<div class="about-member-section">
+			<div class="about-member-post">
+				<span>NSM</span> 
+			</div>
+			<div class="about-member-name">
+				<p>Mr. Vipin Kumar</p>
+			</div>
+		</div>
+	</div>
+	<div class="about-member">
+		<div class="about-member-section">
+			<div class="about-member-post">
+				<span>ZSM</span>
+			</div>
+			<div class="about-member-name">
+				<p>Mr. Vineet Udeniya</p>
+			</div>
+		</div>
+	</div>
+	<div class="about-member">
+		<div class="about-member-section">
+			<div class="about-member-post">
+			 <span>RSM</span>	
+			</div>
+			<div class="about-member-name">
+				<p>Mr. Vikrant Srivastava</p>
+			</div>
+		</div>
+	</div>
+		<div class="about-member">
+		<div class="about-member-section">
+			<div class="about-member-post">
+				<span>ASM</span> 
+			</div>
+			<div class="about-member-name">
+				<p>Mr. Saurabh Vishwa Kumar</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+</div>
+<?php get_footer(); ?>
---
+++ wp-content/themes/meha/page-product-test.php
@@ -0,0 +1,329 @@
+<?php
+/**
+ * Template Name: Product Static Page
+ *
+ * The template for homepage
+ *
+ */
+get_header();
+?>
+
+	     <div class="allproduct">
+          <div class="mcoldplus">
+            <div class="mcoldplud-main">
+             <div class="mcoldplus-section">
+                 <div class="mcoldplus-img">
+                    <img src="/wp-content/uploads/2018/06/mcoldplus.jpg" />
+                 </div>
+                 <div class="mcoldplus-content">
+                    <img src="/wp-content/uploads/2018/06/mcoldplus.png" />
+                    <p>Paracetamol 125 mg, Phenylephrine 5 mg, Chlorpheniramine Maleate 0.5 mg,<br> Sodium Citrate 60 mg $ Menthol 1 mg./5ml Syrup </p>
+                     <h2>Paracetamol</h2>
+                     <p>The World Health Organization (WHO) recommends that Paracetamol be used to treat fever in children in fever is (101.3of) or more</p>
+                     <h3>Phenylephrine</h3>
+                     <p>Phenylephrine is a decongestant that  shrinks blood vessels in the nasal passages</p>
+                     <h4>Chlorpheniraine Maleate</h4>
+                     <p>CPM is used in the prevention of the symptoms of allergic conditions</p>
+                     <h5>Sodium Citrate</h5>
+                     <p>Time tested mucolytic agent</p>
+                     <h6>Menthol</h6>
+                     <p>As a soothing agent relive lrrantion & provides cooling sensation</p>
+              </div>
+        </div>
+     <div class="menzy-plus-indications">
+                   <h1>Indications</h1>
+                 <ul>
+                       <li>Runny Nose</li>
+                       <li>Nasal Congestion</li>
+                       <li>Cold</li>
+                       <li>Allergic Rhinitis</li>
+                       <li>Fever</li>
+                       <li>Sneezing</li>
+                  </ul>
+            </div>     </div> 
+</div>
+<div class="life-up">
+    <div class="life-up-main">
+      <div class="life-up-content">
+          <img src="/wp-content/uploads/2018/06/life-up.png" />
+          <p>Cyproheptadine Hydrochloride 2 mg. & Tricholine Citrate 275 mg./5ml Sorbitol base</p>
+          <h1>Restore the joy of eating food</h1>
+        <ul>
+          <li>Accepted clinical combination for stimulating appetite $ growth</li>
+          <li>Shows stimulstory effects on growth hormones</li>
+          <li>Asthenia, Geriatric Patients, chronic diseases</li>
+          <li>Stimulates Appetite, Promotes Growth</li>
+          <li>The Right support in growing Children</li>
+          <li>Tones up the entire digestive system</li>
+          <li>Helps children gain weight</li>
+          <li>Improves sleep pattern</li>
+         </ul>
+     </div>
+     <div class="life-up-img">
+      <img src="/wp-content/uploads/2018/06/life-up11.jpg" />
+     </div>
+     <div class="menzy-plus-indications">
+                 <h1>Indications</h1>
+                 <div class="life-up-indications-left">
+                   <ul>
+                         <li>Loss of appetite</li>
+                         <li>Underweight children</li>
+                         <li>Weight loss</li>
+                         <li>Intestinal mal absorptin</li>
+                         <li>Anorexia</li>
+                   </ul>
+             </div>
+     </div>  </div> 
+</div>
+<div class="lycoplex-m">
+    <div class="lycoplex-m-main">
+      <div class="lycoplex-m-section">
+       <div class="lycoplex-m-img">
+          <img src="/wp-content/uploads/2018/06/lycoplax.jpg" />
+     </div>
+     <div class="lycomplex-m-content">
+             <img src="/wp-content/uploads/2018/06/lycoplex-m.png" /> 
+          <p>Anti-Oxidant, Multi-vitamin, Multi-Mineral with Lycopene Capsules</p>
+         <ul>
+          <li>Reduces oxidative stress</li>
+          <li>Promotes bone mineral density</li>
+          <li>Promotes early recover from diseases</li>
+          <li>Fights the free radicals damaging effects</li>
+          <li>Exhibits positive effect on growth, energy & appetite</li>
+          <li>Reduces the risk of many diseases like CHD, cancer, male infertillity, hypertension, osteoporsis. etc</li>
+          <li>A powerful antioxidant that helps protect cells from damage</li>
+          <li>Boost the immune system, neutralize free radicals, regulates brain function and provides energy for proper growth of body</li>
+         </ul>
+     </div></div>
+     <div class="menzy-plus-indications">
+                   <h1>Indications</h1>
+                    <ul>
+                         <li>Hastens recovery</li>
+                         <li>General well being</li>
+                         <li>Enhances immunocompetence</li>
+                         <li>Improves appetite</li>
+                         <li>Slows process of ageing</li>
+                         <li>Promotes growth & immunity</li>
+                         <li>Comvalescnce</li>
+                         <li>Retards Diabetic</li>
+                         <li>Combats general weakness</li>
+                    </ul>
+        </div>
+</div>  
+  </div> 
+<div class="lycoplexsyrup-m">
+    <div class="lycoplexsyrup-m-main">
+       <div class="lycoplexsyrup-m-content">
+           <img src="/wp-content/uploads/2018/06/lycoplex-m1.png" />
+           <p>Lycopene, Multivitamin, Multiminerals & Antioxidant Syrup</p>  
+           <h6>Real Lycopene advantage with...<h6>
+           <h2>LYCOPENE</h2>
+          <ul>
+           <li>Increase sperm concentration in the management of idiopathic male infertility</li>
+           <li>No interactions with other circulating carotenoids</li>
+           <li>Lycopene in the prevention & treatment of prostate cancer</li>
+          </ul>
+           <h3>MULTIVITAMINS</h3>
+          <ul>
+           <li>Vital role in the prevention of aging related degenerative diseases</li>
+          </ul>
+           <h4>MULTIMINERALS</h4>
+          <ul>
+           <li>Boosts the immune system</li>
+          </ul>
+           <h5>ANTIOXIDANT</h5>
+          <ul>
+           <li>Promotes early recover from diseases</li>
+          </ul>
+     </div>
+    <div class="lycoplexsyrup-m-img">
+          <img src="/wp-content/uploads/2018/06/WhatsApp-Image-2018-03-09-at-14.22.20-1.jpeg" />
+    </div>
+    <div class="menzy-plus-indications">
+                 <h1>Indications</h1>
+                   <ul>
+                       <li>General Debility</li>
+                       <li>Chronic lllness</li>
+                       <li>Oxidative Stress</li>
+					   <li>Convalescence</li>
+                       <li>Antibiotics</li>
+                       <li>Post Pregnancy</li>
+                   </ul>
+            </div> 
+     </div>
+</div>
+<div class="livron">
+   <div class="livron-main">
+       <div class="livron-section">
+     <div class="livron-img">
+          <img src="/wp-content/uploads/2018/06/livron4.jpg" />
+     </div>
+     <div class="livron-content">
+          <img src="/wp-content/uploads/2018/06/livron.png" />
+           <h1>Healthy Liver for Healthy life</h1>
+          <ul>
+           <li>Possesse antioxidative active</li>
+           <li>Improvers appetite & gain</li>
+           <li>Shorten the disease-recovery period</li>
+           <li>Promotes hepatocellular regeneration</li>
+           <li>Prevents action on spread of hepatitis virus</li>
+           <li>Prevents the loss of functional integrity of call membreances</li>
+           <li>Offers cytoprotection against alcohol-induced hepatic damage</li>
+          </ul>
+     </div>
+  </div>
+     <div class="menzy-plus-indications">
+            <h1>Indications</h1>
+                   <ul>
+                       <li>Chronic Hepatitis</li>
+                       <li>Fatty Liver</li>
+                       <li>Jaundice</li>
+					   <li>Liver Cirrhosis</li>
+                       <li>Hepatic encephalopathy</li>
+                       <li>Post Hepatitis</li>
+                   </ul>
+        </div>  </div>   
+  </div>
+<div class="mehamol-n">
+   <div class="mehamol-n-main">
+     <div class="mehamol-n-content">
+          <img src="/wp-content/uploads/2018/06/mehamol-n.png">
+         <ul>
+          <li>Selective COX-2 inhibitor Anti-inflammtory</li>
+          <li> The risk of liver injury in patients taking Nimesulide and other non-steroidal anti-inflammatory drug in small</li>
+         </ul>
+          <h2>Safe than Diclofenac</h2>
+         <ul>
+          <li>Lasser side effects; comparable efficacy with diclofenac</li>
+         </ul>
+          <h2>Assurance of complete performance</h2>
+         <ul>
+          <li>Relieves pain & skeletal muscle spasms quickly & effectively</li>
+          <li>Restores mobility quickly & effectively</li>
+          <li>Gives comfort in associated cases of fever</li>
+         </ul>
+          <h2>High Speed Analgesic Combination</h2>
+         <ul>
+          <li>Prostaglandin inhibitor at sites of inflammation</li>
+          <li>Convenient BIN Dosage</li>
+          <li>Rapid Absorption Synovium</li>
+          <li>Reduced Tissue Damage</li>
+          <li>Less Gastric Lrrtation</li>
+          <li>Safe in Asthmatics</li>
+         </ul>
+          <h1>... Power for Speedy Results</h1>
+     </div>
+     <div class="mehamol-n-img">
+       <img src="/wp-content/uploads/2018/06/IMG-20180405-WA0017-Copy-2-1.jpg" />
+     </div>
+  </div>
+</div>
+<div class="menzy-plus">
+ <div class="menzy-plus-main">
+  <div class="menzy-plus-section">
+     <div class="menzy-plus-img">
+           <img src="/wp-content/uploads/2018/06/manzela-plus.jpg" />
+      </div>
+       <div class="menzel-plus-content">
+            <img src="/wp-content/uploads/2018/06/manzel.png" />
+            <h5>Fortified with Digestive Enzymes, Vitamins B-Complex & Minerals Syrup</h5>
+          <ul>
+            <li>Natural pro-digestive enzyme, used in disturbances of G I function & Prevention of malabsorption of food</li>
+            <li>A proteolytic enzyme which breaks down proteins & Peptides into simpler molecules</li>
+            <li>Very effective for Indigestion,Nausea, Vomiting, upset stomach, diarrhea & Faltulence</li>
+            <li>Restores the natural digestive system</li>
+            <li>Controls symptoms like belching, bloating & Flatulence</li>
+            <li>Increases bile flow, whicb improves the digestion of fats</li>
+            <li>Beneficial for treating colic, decreasing smooth muscle cramping and griping pain(Intestinal Cramping)</li>
+          </ul>
+       </div>     
+    </div> 
+        <div class="menzy-plus-indications">
+            <h1>Indications</h1>
+                   <ul>
+                        <li>Chronic Hepatitis</li>
+                        <li>Fatty Liver</li>
+                        <li>Jaundice</li>
+                        <li>Liver Cirrhosis</li>
+                       <li>Post Hepatitis</li>
+                   </ul>
+     </div>    </div> 
+</div>
+<div class="broncodyl">
+   <div class="broncodyl-main">
+     <div class="broncodyl-section">
+          <div class="broncodyl-content">
+               <img src="/wp-content/uploads/2018/06/bronodyl.png" />
+               <spam>Bromhexine 2 mg, Terbutaline 1.25 mg, & Guaiphenesin 100 mg/5ml Syrup</spam>
+               <h1>Bromhexine</h1>
+               <p>Potent inhibitor of the neuronal Na+ channels Secretolytic agent provides relief in pharyngitis</p>
+               <h2>Terbutaline</h2>
+               <p>Fast acting bronchodilator, opens air passages in the lungs, making it easier to breathe</p>
+               <p>Actively treats symptoms like wheezing,shortness of breath and troubled breathing</p>
+               <h3>Guaiphenesin</h3>
+               <P>Reduces the viscostiy of secretion</p>
+               <p>Increases the efficiency of the cough reflex</p>
+         </div>
+         <div class="broncodyl-img">
+               <h1>Powerful combination of expectorant and Anti-Histamine</h1>
+               <h2>Easy BREATHING Every time</h2>
+               <img src="/wp-content/uploads/2018/06/bronodyl1.jpg" />
+         </div>
+     <div class="menzy-plus-indications">
+            <h1>Indications</h1>
+                   <ul>
+                         <li>Common cough & cold</li>
+                         <li>Cough & throat irritation</li>
+                        <li>Asthma</li>
+                        <li>Bronchitis</li>
+                       <li>Dyspnea & Sinusitis</li>
+                       <li>Smokes's Cough</li>
+                   </ul>
+     </div> 
+  </div>  </div>
+</div>
+<div class="mehaflox-oz">
+    <div class="mehaflox-oz-main">
+       <div class="mehaflox-oz-section">
+         <div class="mehaflox-oz-img">
+               <p>Complete <spam>Proctection</spam></p>
+               <p>Against <spam>Multiple </spam>Infection</p>
+              <img src="/wp-content/uploads/2018/06/mehaflox-oz-2.jpg" />
+         </div>
+         <div class="mehaflox-oz-content">
+              <img src="/wp-content/uploads/2018/06/mehaflox-oz.png" />
+              <h1>Ofloxacin</h1>
+              <ul>
+                  <li>Ofloxacin is a safe bet to treat Serious infection in Children.</li>
+                  <li>Balanced coverage against aerobic, Anaerobic & mixed infections</li>
+              </ul>
+              <h2>Ornidazole</h2>
+              <ul>
+                  <li>Ornidazole is a nitromidazole which is an antibacterial and antiprotozoal drug used to treat anaerobic enteric Protozoa</li>
+                <li>It is a drug cures some protozoan infextion like (Giardiasis, Trichomoniasis)</li>
+               <li>This Antibacterial agent effective against a wide variety fo gram-ve & gram+ve aerobic bacteria</li>
+           </ul>
+         </div>
+   </div>
+    <div class="menzy-plus-indications">
+            <h1>Effective In</h1>
+                   <ul>
+                         <li>G.I. Infections</li>
+                         <li>Traveller's Diarrhoea</li>
+                        <li>Enteric Fever</li>
+                        <li>Acute & chronic Diarrhoea</li>
+                       <li>Shigellosis</li>
+                       <li>Inflammatory Bowel Syndrom</li>
+                       <li>Urinary Tract Infections</li>
+                   </ul>
+     </div>  
+</div>
+</div>
+</div>
+
+
+
+
+
+		
+<?php get_footer(); ?>
